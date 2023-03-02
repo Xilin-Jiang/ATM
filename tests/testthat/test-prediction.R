9de7522bf39a0b77d87d605d2ae662b100cefafc
Xilin-Jiang <xilin@well.ox.ac.uk>
2023-03-02 19:00:49 +0000

    finish function predict_OR for prediction odds ratio

--- tests/testthat/test-prediction.R
+++ tests/testthat/test-prediction.R
@@ -3,8 +3,8 @@ test_that("test prediction odds ratio", {
   disease_list <- ATM::UKB_349_disease
   testing_data <- ATM::HES_age_example %>%
     slice(1:10000)
-  load("../../comorbidity/Results/Run_2rec_PheCode_age_dependent_K10_P5_rep10.RData")
-  para_training <- para
+  para_training <- topic_init_age(ATM::HES_age_example, ds_list = ATM::UKB_349_disease,topic_num = 10, degree_free_num = 5)
+  para_training$pi_beta_basis <- ATM::UKB_HES_10topics
   output_old <- prediction_onebyone(testing_data, ds_list = disease_list, para_training, max_predict = 5)
   new_output <- prediction_OR(testing_data, ds_list = disease_list, topic_loadings =  topic_loadings, max_predict = 5)
   expect_equal(topic_loadings, para_training$pi_beta_basis)
