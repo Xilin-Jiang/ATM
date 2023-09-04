cbb6758536c7ff7349cba2c730bcecc549404c3c
Xilin-Jiang <xilin@well.ox.ac.uk>
2023-09-04 01:13:54 +0100

    update ATM lfa method, using more terms in taylor expansion during the approximation

--- tests/testthat/test-lfa.R
+++ tests/testthat/test-lfa.R
@@ -63,14 +63,4 @@ test_that("lfa accuracy", {
   # plot inferred topics:
   topics_inf <- simu_result$topic_loadings
 
-  para <- topic_init_lfa_cvb(rec_data = HES_age_example, ds_list=UKB_349_disease, topic_num=10)
-  # update the zn; each step should increase the lower bound
-  lb1 <- CVB_lb_lfa(para)
-  para <- CVB0_E_zn(para)
-  lb2 <- CVB_lb_lfa(para)
-  expect_gt(lb2, lb1)
-  # update the beta
-  para <- update_beta_lfa(para)
-  lb3 <- CVB_lb_lfa(para)
-  expect_gt(lb3, lb2)
 })
